837577996116328faf54d4edcd29f0ea46c7e37c
Erick Moore <erick.moore@microsoft.com>
2022-01-13 15:27:14 -0500

    script updates

--- Bicep/Scripts/rdpshortpath.ps1
+++ Bicep/Scripts/rdpshortpath.ps1
@@ -1,12 +1,20 @@
-$rdpGpoURI      = "https://aka.ms/avdgpo"
-$rdpGpoFileName = "AVDGPTemplate.cab"
-$outputPath     = "C:\temp"
-$outputFile     = "AVDGPTemplate.zip"
+$WinstationsKey = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations'
 
-Invoke-WebRequest -Uri $rdpGpoURI -UseBasicParsing -OutFile $rdpGpoFileName
-cmd.exe /c "C:\Windows\System32\expand.exe $rdpGpoFileName  -F:* $outputPath\$outputFile"
+if(Test-Path $WinstationsKey){
+    New-ItemProperty -Path $WinstationsKey -Name 'fUseUdpPortRedirector' -ErrorAction:SilentlyContinue -PropertyType:dword -Value 1 -Force
+    New-ItemProperty -Path $WinstationsKey -Name 'UdpPortNumber' -ErrorAction:SilentlyContinue -PropertyType:dword -Value 3390 -Force
+}
 
-Expand-Archive -Path "$outputPath\$outputFile" -DestinationPath $outputPath -Force
-
-Copy-Item -Path "$outputPath\terminalserver-avd.admx" -Destination $env:WINDIR\PolicyDefinitions
-Copy-Item -Path "$outputPath\en-us\terminalserver-avd.adml" -Destination $env:WINDIR\PolicyDefinitions\en-us
+New-NetFirewallRule ` 
+ -DisplayName 'Remote Desktop - Shortpath (UDP-In)' `
+ -Action Allow `
+ -Description 'Inbound rule for the Remote Desktop service to allow RDP traffic. [UDP 3390]' `
+ -Group '@FirewallAPI.dll,-28752' `
+ -Name 'RemoteDesktop-UserMode-In-Shortpath-UDP'  `
+ -PolicyStore PersistentStore `
+ -Profile Domain, Private `
+ -Service TermService `
+ -Protocol udp `
+ -LocalPort 3390 `
+ -Program '%SystemRoot%\system32\svchost.exe' 
+ `-Enabled:True
